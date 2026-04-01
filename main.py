ce38e601e95ea7b5664af423be19471467a78282
Vishesh Kathuria <vishesh.kathuria2004@gmail.com>
2026-04-01 23:35:10 +0530

    Addition of Duplicate detection and Updated the Readme

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from typing import Annotated
-import typer
+import typer  
 import pathlib
 import hashlib
+from collections import defaultdict
+
 
 app = typer.Typer()
 
 def scan(folder: str):
     p = pathlib.Path(folder)
+    hash_store = defaultdict(list)
     for f in p.rglob(f"*"):
         if f.is_file():
             try:
@@ -14,12 +17,21 @@ def scan(folder: str):
                 with f.open('rb') as handle:
                     content = handle.read()
                     h.update(content)
-                    print(h.hexdigest())
+                    file_hash = h.hexdigest()
+                    #Mapping the file_hash as key and f as the value
+                    hash_store[file_hash].append(f)
             except PermissionError :
                 print(f"SKIPPING: {f.name} (Permission Denied)")
 
             except Exception as e:
                 print(f"ERROR: Could not scan {f.name}: {e}")
+    
+    for key,value in hash_store.items():
+        if len(value) > 1:
+            print(f"\n[!] Duplicate Group Found")
+            print(f"Duplicates found for the key: {key}")
+            for path in value:
+                print(f"The values are  -> : {path}")
 
 
 
@@ -37,3 +49,4 @@ def main(
 
 if __name__ == "__main__":
     app()
+
